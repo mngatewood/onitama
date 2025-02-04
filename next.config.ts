6ba45e01fc6fae9be28f0d8e6eae6abcb920006a
Michael Gatewood <michael@coincloudnv.com>
2025-02-04 13:35:06 -0800

    Hide Next.js dev indicators for testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone",
+	// output: "standalone",
+	devIndicators: {
+		buildActivity: false,
+		appIsrStatus: false,
+	},
 };
 
 export default nextConfig;
