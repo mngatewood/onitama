b2071c8e25e578d9cb1d56c223b4773dd41b588b
Michael Gatewood <michael@coincloudnv.com>
2025-02-04 13:17:54 -0800

    Add dark mode and toggle component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
 import spirit from "../public/spirit.png";
+import { DarkModeToggle } from "./components/DarkThemeToggle";
 
 export default function Home() {
+
 	return (
 		<div>
 			<main className="fixed top-0 left-0 right-0 bottom-0 overflow-hidden flex justify-center z-10">
-				<h1 className="dark text-4xl text-center m-8 font-reggae">Onitama</h1>
-				<footer className="container absolute bottom-0 w-full h-14 p-4 flex justify-center gap-4">
-					<button className="dark w-1/3"><a href="/login">Login</a></button>
-					<button className="dark w-1/3"><a href="/register">Register</a></button>
-					<button className="dark w-1/3"><a href="/demo">Guide</a></button>
+				<h1 className="text-4xl text-center text-blue-1 dark:text-yellow-1 m-8 font-reggae">Onitama</h1>
+				<footer className="container absolute bottom-0 w-full h-14 p-4 flex justify-center gap-4 text-sky-700 dark:text-sky-300">
+					<button className="w-1/3"><a href="/login">Login</a></button>
+					<button className="w-1/3"><a href="/register">Register</a></button>
+					<button className="w-1/3"><a href="/demo">Guide</a></button>
 				</footer>
 			</main>
 			<div className="absolute flex w-full h-screen items-center justify-center z-0">
-				<Image src={spirit} width={640} height={640} alt="Spirit" className="w-full max-w-[640px]" />
+				<Image src={spirit} width={640} height={640} alt="Spirit" className="w-full max-w-[640px]" priority />
 			</div>
+			<DarkModeToggle />
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,18 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        yellow: {
+          1: "#fce67d",
+        },
+        blue: {
+          1: "#01092b",
+        },
       },
       fontFamily: {
         sans: ["var(--font-geist-sans)", "sans-serif"],
@@ -21,3 +28,4 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
