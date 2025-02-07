6e5aa4fd7a24e5288b3631df5a6aa428da67784b
Michael Gatewood <michael@coincloudnv.com>
2025-02-07 08:14:22 -0800

    - Add logout component - Update tests

---
+++ app/components/forms/LogoutForm.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export const LogoutForm = () => {
+
+	const handleSignOut = async (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		await signOut();
+		redirect('/');
+	};
+		
+	return (
+		<form className="mt-8">
+			<button onClick={handleSignOut} className="group/btn relative block h-10 mb-8 w-full rounded-md bg-gradient-to-br from-neutral-500 to-neutral-400 font-medium hover:bg-gradient-to-br hover:from-neutral-600 hover:to-neutral-400 text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:hover:bg-white dark:hover:from-zinc-800 dark:hover:to-zinc-700 dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-800 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] disabled:pointer-events-none disabled:opacity-75">
+				Logout
+			</button>
+		</form>
+	);
+}
--- tests/logout.test.ts
+++ tests/logout.test.ts
@@ -1,42 +1,96 @@
 import { expect, test } from '@playwright/test';
 
+const localhost = 'http://localhost:3000/';
+const getEmail = () => `${Date.now()}@tested.com`
+const email = getEmail();
+
 test.describe('user can logout', () => {
 
+	test.beforeAll(async ({ page }) => {
+		// Register a user
+		await page.goto('/register');
+		await page.waitForTimeout(500);
+		await page.locator('#firstName').fill('Test');
+		await page.locator('#lastName').fill('User');
+		await page.locator('#email').fill(email);
+		await page.locator('#password').fill('password');
+		await page.locator('#confirmPassword').fill('password');
+		await page.getByRole('button', { name: 'Submit' }).click();
+		await page.waitForTimeout(1000);
+		await page.getByRole('button', { name: 'Proceed' }).click();
+	});
+
 	test.beforeEach(async ({ page }) => {
-		test.skip();
+		// Login the user
 		await page.goto('/login');
+		await page.waitForTimeout(500);
+		await page.locator('#email').fill(email);
+		await page.locator('#password').fill('password');
+		await page.getByRole('button', { name: 'Submit' }).click();
+		await page.waitForTimeout(1000);
+		await page.getByRole('button', { name: 'Proceed' }).click();
+		await page.goto('/');
+
 	});
 
 	test('take a screenshot', async ({ page }, workerInfo) => {
 		await page.screenshot({ path: `./test-results/logout.${workerInfo.project.name}.png` });
 	});
 
-	test('account icon is visible when in the game lobby', async ({ page }) => {
-
+	test('logout icon is visible from the home page when the user is logged in', async ({ page }) => {
+		await expect(page.getByRole('heading', { name: 'Onitama' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Switch to light / dark version' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Register' })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Login' })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Guide' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
 	});
 
-	test('When the user clicks the account icon, the account management modal is displayed', async ({ page }) => {
-
+	test('when the user clicks the Logout button from the home page, a modal appears', async ({ page }) => {
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		await expect(page.getByRole('heading', { name: 'Logout', exact: true })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Guide' })).not.toBeVisible();
+	});
+	
+	test('when the user clicks the Cancel button from the logout modal, the logout modal closes', async ({ page }) => {
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		await page.getByRole('button', { name: 'Cancel' }).click();
+		await page.waitForTimeout(500);
+		await expect(page.getByRole('heading', { name: 'Logout', exact: true })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Cancel' })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Guide' })).toBeVisible();
 	});
 
-	test('account management modal displays a Logout button', async ({ page }) => {
-
-	});
-
-	test('when the user clicks the Logout button, a modal appears', async ({ page }) => {
-
-	});
-
-	test('when the user clicks the Logout button, the user is logged out', async ({ page }) => {
-
+	test('when the user clicks the Logout button from the logout modal, the user is logged out', async ({ page }) => {
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		await page.goto('/login');
+		expect(page.url()).toBe(localhost + 'login');
+		await page.goto('/register');
+		expect(page.url()).toBe(localhost + 'register');
+		await page.goto('/logout');
+		expect(page.url()).toBe(localhost);
 	});
 
 	test('when the user is logged out, they are redirected to the home page', async ({ page }) => {
-
-	});
-
-	test('when the user clicks the Cancel button, the account management modal closes', async ({ page }) => {
-
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		await page.getByRole('button', { name: 'Logout' }).click();
+		await page.waitForTimeout(500);
+		expect(page.url()).toBe(localhost);
+		await expect(page.getByRole('heading', { name: 'Logout', exact: true })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Cancel' })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Logout' })).not.toBeVisible();
+		await expect(page.getByRole('button', { name: 'Register' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Guide' })).toBeVisible();
 	});
 
 });
