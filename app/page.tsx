97f782261945087388753022c1794301bbf5eaab
Michael Gatewood <michael@coincloudnv.com>
2025-02-07 13:56:40 -0800

    Remove lobby code from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import { getServerSession } from "next-auth";
 import Home from "./components/Home";
-import Lobby from "./components/Lobby";
 
 const Index = async () => {
-	const session = await getServerSession();
 
 	return (
-		<>
-			{!!session ? (
-				<Lobby />
-			) : (
-				<Home />
-			)}
-		</>
-	);
+		<Home />
+	)
 }
 
 export default Index;
