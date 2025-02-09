01220c441206f8b6994734744acc9f33a0b74b3c
Michael Gatewood <michael@coincloudnv.com>
2025-02-09 14:08:53 -0800

    Update tests

--- tests/newGame.test.ts
+++ tests/newGame.test.ts
@@ -1,27 +1,29 @@
 import { expect, test } from '@playwright/test';
+import { localhost, getEmail, registerUser, loginUser } from './test-helpers';
+
+const email = getEmail();
 
 test.describe('user can create a new game', () => {
 
+	test.beforeAll(async ({ page }) => {
+		await registerUser({ page }, email);
+	});
+
 	test.beforeEach(async ({ page }) => {
-		test.skip();
-		await page.goto('/login');
-	});
-
-	test('take a screenshot', async ({ page }, workerInfo) => {
-		await page.screenshot({ path: `./test-results/newGame.${workerInfo.project.name}.png` });
-	});
-
-	test('when a user joins the game lobby, all pending games older than 15 minutes are deleted', async ({ page }) => {
-
+		await loginUser({ page }, email);
 	});
 
 	test('when the user is in the game lobby, a New Game button is displayed', async ({ page }) => {
-
+		await page.goto('/');
+		await expect(page.getByRole('button', { name: 'New Game' })).toBeVisible();
 	})
 
 	test('when the New Game button is clicked, a new game is added to the database', async ({ page }) => {
-
-	})
+		await page.goto('/');
+		await page.getByRole('button', { name: 'New Game' }).click();
+		await page.waitForTimeout(500);
+		await expect(page.getByText('Waiting for another player to join...')).toBeVisible();
+	});
 
 	test('when a game is created, the game page is displayed with a modal', async ({ page }) => {
 
@@ -39,5 +41,8 @@ test.describe('user can create a new game', () => {
 
 	})
 
+	test('when a user joins the game lobby, all pending games older than 15 minutes are deleted', async ({ page }) => {
+
+	});
 
 });
