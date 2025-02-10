1b85a8209b9b5ca7e7d81b1d51216e155998d169
Michael Gatewood <michael@coincloudnv.com>
2025-02-10 15:23:44 -0800

    - Hide welcome modal when second player joins (pending two-player implementation) - Update tests

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -2,45 +2,55 @@
 
 import { useEffect, useState } from "react";
 import { WaitingModal } from "./WaitingModal";
+import { Board } from "./Board";
+
+interface Game {
+	id: string;
+	users: string[];
+	board: string[][];
+}
 
 export const Game = ({ gameId }: { gameId: string }) => {
-	const [game, setGame] = useState(null);
+	const [game, setGame] = useState<Game | null>(null);
 	const [waiting, setWaiting] = useState(true);
 
-	const fetchGame = async (id: string) => {
-		const response = await fetch(`/api/games/?id=${id}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-
-		setGame(await response.json());
-	};
-
+	
+	
 	useEffect(() => {
+		const fetchGame = async (id: string) => {
+			const response = await fetch(`/api/games/?id=${id}`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const gameData: Game = await response.json();
+			setGame(gameData);
+			if (gameData.users.length === 2) {
+				setWaiting(false);
+			} else {
+				// change to false for development until 2nd player is implemented
+				setWaiting(true);
+			}
+		};
 		if (gameId) {
 			fetchGame(gameId);
 		}
 	}, [gameId]);
 	console.log(game)
 
-	if (!game) {
-		return (
-			<WaitingModal text="Loading..." />
-		)
-	}
-
 	return (
 		<>
+			{game && game.board && <Board board={game.board} />}
 			{!game &&
-				 <WaitingModal text="Waiting for another player to join..." />
-			}
-			{game && waiting &&
-				<WaitingModal text="Waiting for another player to join..." />
-			}
-			{game && !waiting &&
-				<h1>Game</h1>
+				<div className="absolute top-0 left-0 right-0 bottom-0 flex items-center">
+					<WaitingModal text="Loading game..." />
+				</div>
+				}
+				{game && waiting &&
+				<div className="absolute top-0 left-0 right-0 bottom-0 flex items-center">
+					<WaitingModal text="Waiting for another player to join..." />
+				</div>
 			}
 		</>
 	)
--- tests/newGame.test.ts
+++ tests/newGame.test.ts
@@ -18,31 +18,20 @@ test.describe('user can create a new game', () => {
 		await expect(page.getByRole('button', { name: 'New Game' })).toBeVisible();
 	})
 
-	test('when the New Game button is clicked, a new game is added to the database', async ({ page }) => {
+	test('when a game is created, the game page is displayed with a modal', async ({ page }) => {
 		await page.goto('/');
 		await page.getByRole('button', { name: 'New Game' }).click();
 		await page.waitForTimeout(500);
 		await expect(page.getByText('Waiting for another player to join...')).toBeVisible();
-	});
-
-	test('when a game is created, the game page is displayed with a modal', async ({ page }) => {
-
-	})
-
-	test('the modal reads “Waiting for another player to join.”', async ({ page }) => {
-
-	})
-
-	test('the modal displays an Exit Game button', async ({ page }) => {
-
+		await expect(page.getByRole('button', { name: 'Exit Game' })).toBeVisible();
 	})
 
 	test('when the user clicks the Exit Game button, they are redirected to the game lobby', async ({ page }) => {
-
+		// TODO
 	})
 
 	test('when a user joins the game lobby, all pending games older than 15 minutes are deleted', async ({ page }) => {
-
+		// TODO
 	});
 
 });
