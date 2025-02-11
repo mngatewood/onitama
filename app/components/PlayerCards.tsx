e3fda0f0a5747da152a8eec1d73c96a5abbf47a2
Michael Gatewood <michael@coincloudnv.com>
2025-02-11 10:13:23 -0800

    Fix bugs from build

--- app/components/PlayerCards.tsx
+++ app/components/PlayerCards.tsx
@@ -10,7 +10,7 @@ export const PlayerCards = ({player}: {player: Player}) => {
 	const playerColor = player.color === "red" ? "bg-red-900" : "bg-blue-900";
 	return (
 		<div className="player w-full h-full flex justify-between">
-			<div className={`${playerColor} player-color w-6 h-10 border rounded-xl border-neutral-400 bg-neutral-400 flex justify-center items-center text-lg text-gray-300 ml-1 mr-3`}>
+			<div className={`${playerColor} player-color w-6 h-10 border rounded-xl border-neutral-400 flex justify-center items-center text-lg text-gray-300 ml-1 mr-3`}>
 				{playerKanji}
 			</div>
 			<div className="w-full flex justify-between px-2">
