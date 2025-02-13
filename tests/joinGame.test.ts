00beea258ba222be21161e71420f03caf9eec21e
Michael Gatewood <michael@coincloudnv.com>
2025-02-13 08:29:08 -0800

    Update join game tests

--- tests/joinGame.test.ts
+++ tests/joinGame.test.ts
@@ -1,26 +1,102 @@
 import { expect, test } from '@playwright/test';
+import { localhost, getEmail, registerUser, loginUser, createGame, logoutUser, clearTestData } from './test-helpers';
+
+const email = getEmail();
+const emailTwo = "2" + email
+const emailThree = "3" + email
+const emailFour = "4" + email
+
+test.describe.configure({ mode: 'parallel' });
 
 test.describe('user can join a game', () => {
 
 	test.beforeEach(async ({ page }) => {
-		test.skip();
-		await page.goto('/login');
+		await clearTestData();
+		await registerUser({ page }, email);
+		await registerUser({ page }, emailTwo);
+		await registerUser({ page }, emailThree);
+		await registerUser({ page }, emailFour);
+		await page.goto('/');
+	});
+
+	test.afterEach(async ({ page }) => {
+		await logoutUser({ page });
+		await page.waitForTimeout(500);
 	});
 
 	test('take a screenshot', async ({ page }, workerInfo) => {
+		await loginUser({ page }, email);
 		await page.screenshot({ path: `./test-results/joinGame.${workerInfo.project.name}.png` });
 	});
 
-	test('when the user is in the game lobby, a list of pending games is displayed', async ({ page }) => {
-
+	test('when the first user is in the game lobby, there should not be any test games', async ({ page }) => {
+		await loginUser({ page }, email);
+		await expect(page.getByText('TestW@@+')).not.toBeVisible();
 	});
 
-	test('the list of pending games is sorted from oldest to newest', async ({ page }) => {
+	test('when the user is in the game lobby, a list of pending games is displayed', async ({ page }) => {
+		await loginUser({ page }, email);
+		await createGame({ page });
+		await logoutUser({ page });
+		await loginUser({ page }, emailTwo);
+		await page.waitForTimeout(500);
+		await expect(page.getByText('TestW@@+')).toBeVisible();
+		await expect(page.getByText('TestW@@+')).toHaveCount(1);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toBeVisible();
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toHaveCount(1);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" }).getByRole("link")).toHaveCount(1);
+	});
 
+	test('the list of pending games is sorted from newest to oldest', async ({ page }) => {
+		await loginUser({ page }, email);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toHaveCount(0);
+		await createGame({ page });
+		await logoutUser({ page });
+		await loginUser({ page }, emailTwo);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toHaveCount(1);
+		await createGame({ page });
+		await logoutUser({ page });
+		await loginUser({ page }, emailThree);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toHaveCount(2);
+		await createGame({ page });
+		await logoutUser({ page });
+		await loginUser({ page }, emailFour);
+		await page.waitForTimeout(500);
+		await expect(page.getByText('TestW@@+')).toHaveCount(3);
+		await expect(page.locator('.game-join-entry').filter({ hasText: "TestW@@+" })).toHaveCount(3);
+
+		const [first, second, third] = await Promise.all([
+			page.locator('div').filter({ hasText: /^TestW\@\@\+/ }).locator('.game-created-at').nth(0).textContent(),
+			page.locator('div').filter({ hasText: /^TestW\@\@\+/ }).locator('.game-created-at').nth(1).textContent(),
+			page.locator('div').filter({ hasText: /^TestW\@\@\+/ }).locator('.game-created-at').nth(2).textContent(),
+		]);
+
+		if (first !== null && second !== null && third !== null) {
+			const firstDate = new Date(first);
+			const secondDate = new Date(second);
+			const thirdDate = new Date(third);
+			expect(firstDate > secondDate && secondDate > thirdDate).toBe(true);
+		} else {
+			expect(false).toBe(true);
+		}
 	});
 
 	test('when the user clicks the Join button for a pending game, the game page is displayed', async ({ page }) => {
-
+		await loginUser({ page }, email);
+		await createGame({ page });
+		await logoutUser({ page });
+		await loginUser({ page }, emailTwo);
+		await page.locator('.game-join-entry').filter({ hasText: "TestW@@+" }).getByRole("link").click();
+		await page.waitForTimeout(500);
+		await expect(page.locator('#board')).toBeVisible();
+		await expect(page.locator('#board')).toHaveCount(1);
+		await expect(page.locator('.space')).toHaveCount(25);
+		await expect(page.locator('.not-highlighted')).toHaveCount(25);
+		await expect(page.locator('.red.student')).toHaveCount(4);
+		await expect(page.locator('.red.master')).toHaveCount(1);
+		await expect(page.locator('.blue.student')).toHaveCount(4);
+		await expect(page.locator('.blue.master')).toHaveCount(1);
+		// TODO: cards should be dealt and turn order determined
 	});
 
 	test('when another user joins the game, waiting for another player modal closes', async ({ page }) => {
--- tests/test-helpers.ts
+++ tests/test-helpers.ts
@@ -1,31 +1,76 @@
 import { Page } from '@playwright/test';
+import { prisma } from "../app/lib/prisma";
 
 export const localhost = 'http://localhost:3000/';
 
 export const getEmail = () => `${Date.now()}@tested.com`
 
+const deleteOldTestGames = async () => {
+	await prisma.game.deleteMany({
+		where: {
+			users: {
+				some: {
+					first_name: {
+						equals: "TestW@@+"
+					}
+				}
+			},
+		}
+	});
+}
+
+const deleteOldTestUsers = async () => {
+	await prisma.user.deleteMany({
+		where: {
+			first_name: {
+				equals: "TestW@@+"
+			},
+		}
+	});
+}
+
+export const clearTestData = async () => {
+	await deleteOldTestGames();
+	await deleteOldTestUsers();
+}
+
 export const registerUser = async ( { page }: { page: Page }, email: string) => {
 	await page.goto('/register');
 	await page.waitForTimeout(500);
-	await page.locator('#firstName').fill('Test');
+	await page.locator('#firstName').fill('TestW@@+');
 	await page.locator('#lastName').fill('User');
 	await page.locator('#email').fill(email);
 	await page.locator('#password').fill('password');
 	await page.locator('#confirmPassword').fill('password');
 	await page.getByRole('button', { name: 'Submit' }).click();
-	await page.waitForTimeout(1000);
+	await page.waitForTimeout(500);
 	await page.getByRole('button', { name: 'Proceed' }).click();
 	return;
 }
 
 export const loginUser = async ({ page }: { page: Page }, email: string) => {
+	console.log("logging in user", email);
 	await page.goto('/login');
 	await page.waitForTimeout(500);
 	await page.locator('#email').fill(email);
 	await page.locator('#password').fill('password');
 	await page.getByRole('button', { name: 'Submit' }).click();
-	await page.waitForTimeout(1000);
+	await page.waitForTimeout(500);
 	await page.getByRole('button', { name: 'Proceed' }).click();
-	await page.goto('/');
 	return;
 };
+
+export const createGame = async ({ page }: { page: Page }) => {
+	await page.goto('/');
+	await page.waitForTimeout(500);
+	await page.getByRole('button', { name: 'New Game' }).click();
+	await page.waitForTimeout(500);
+	return;
+};
+
+export const logoutUser = async ({ page }: { page: Page }) => {
+	await page.goto('/logout/go');
+	await page.waitForTimeout(500);
+	return;
+};
+
