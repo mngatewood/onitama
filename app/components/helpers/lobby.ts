43892ba75e8c324ae7f8cec02e5c5970b041d227
Michael Gatewood <michael@coincloudnv.com>
2025-02-18 20:19:53 -0800

    - Add try/catch blocks to getPendingGames method - Add joinGame method

--- app/components/helpers/lobby.ts
+++ app/components/helpers/lobby.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/app/lib/prisma";
 import * as utility from "./utility";
 import { Card } from "@prisma/client";
 
+const apiUrl = process.env.API_URL || "http://localhost:3000";
+
 const startingBoard = [
 	["rs0", "rs0", "rm0", "rs0", "rs0"],
 	["000", "000", "000", "000", "000"],
@@ -12,7 +14,8 @@ const startingBoard = [
 
 export const getAllCards = async () => {
 	try {
-		const response = await fetch('/api/cards');
+		const url = `${apiUrl}/cards`;
+		const response = await fetch(url);
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`);
 		}
@@ -56,8 +59,9 @@ export const createGame = async (playerId: string) => {
 		status: status,
 		players: players,
 	}
-	const response = await fetch('/api/games', {
-		method: 'POST',
+	const url = `${apiUrl}/games`;
+	const response = await fetch(url, {
+		method: 'PUT',
 		headers: {
 			'Content-Type': 'application/json',
 		},
@@ -72,6 +76,65 @@ export const createGame = async (playerId: string) => {
 }
 
 export const getPendingGames = async () => {
-	const games = await prisma.game.findMany();
-	return games;
+	try {
+		const url = `${apiUrl}/games?status=waiting_for_players`;
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error fetching pending games:', error);
+		throw error;
+	}
 }
+
+export const joinGame = async (playerId: string, game: Game) => {
+	if(game.users.length !== 1) {
+		return
+	}
+	try {
+		const playerColor = game.players.red.id.length ? "blue" : "red";
+		const opponentColor = playerColor === "red" ? "blue" : "red";
+		const playersUpdate = {
+			[playerColor]: {
+				id: playerId,
+				cards: game.players[playerColor].cards,
+			},
+			[opponentColor]: {
+				id: game.players[opponentColor].id,
+				cards: game.players[opponentColor].cards,
+			}
+		}
+		const update = {
+			gameId: game.id,
+			userId: playerId,
+			players: playersUpdate
+		}
+		const url = `${apiUrl}/games?id=${game.id}&action=join`;
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(update),
+		})
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.json();
+		const updated = 
+			data.players.red.id === playersUpdate.red.id && 
+			data.players.blue.id === playersUpdate.blue.id && 
+			data.users.length === 2;
+		if(!updated) {
+			throw new Error(`Update returned ok but the game was not updated!`);
+		}
+		return data;
+	} catch (error) {
+		console.error('Error joining the game:', error);
+		throw error;
+	}
+}
+
