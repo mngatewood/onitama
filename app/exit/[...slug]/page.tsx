00b69660868cc1c5279a40e220b8b077f48c09db
Michael Gatewood <michael@coincloudnv.com>
2025-02-19 16:29:18 -0800

    - Display notification if game not found in exit route - Redirect to lobby after notification

--- app/exit/[...slug]/page.tsx
+++ app/exit/[...slug]/page.tsx
@@ -4,39 +4,50 @@ import { DarkModeToggle } from "@/app/components/DarkThemeToggle";
 import { ExitForm } from "@/app/components/forms/ExitForm";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { PageProps } from "@/.next/types/app/layout";
+import PageProps from "next/app";
 import { getGame } from "@/app/components/helpers/lobby";
 import { Game } from "@/app/components/Game";
 import { authOptions } from "@/app/api/auth/[...nextauth]/config";
+import { ToastMessage } from "@/app/components/ToastMessage";
 
 interface ParamsObject {
 	slug: string;
 }
 
 const Exit = async ({ params }: PageProps) => {
-
 	const session = await getServerSession(authOptions) as AppendedSession;
 	const paramsObject:ParamsObject = await params;
 	const gameId = paramsObject.slug[0];
 	const game: Game = await getGame(gameId);
-	const user = game.users.find((user) => user.id === session?.user.id);
+	const user = game?.users && game.users.find((user) => user.id === session?.user.id);
+	const notification = {
+		type: "error",
+		message: "Game not found.  Redirecting to the lobby...",
+		timeout: 3000,
+		action: "/"
+	};
 
 	if (!session) {
 		redirect('/login');
 	}
+
 	return (
 		<div className="absolute top-0 left-0 right-0 bottom-0 overflow-hidden flex flex-col justify-center items-center">
 			<header>
 				<Title />
 			</header>
 			<main className="p-4 overflow-hidden flex flex-col items-center my-auto">
-				<div className="mx-auto overflow-y-scroll scroll-no-bars w-full max-w-md rounded-none border border-gray-300 bg-amber-50 px-4 pt-4 shadow-lg shadow-slate-500 dark:shadow-slate-800 dark:border-gray-800 dark:bg-black md:rounded-2xl md:px-8 md:pt-8">
-					<h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">Exit Game</h2>
-					<p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-						Are you sure you want to exit the game?  Any progress will be lost.
-					</p>
-					<ExitForm gameId={gameId} firstName={user?.first_name ?? ""}/>
-				</div>
+				{ game ?
+					<div className="mx-auto overflow-y-scroll scroll-no-bars w-full max-w-md rounded-none border border-gray-300 bg-amber-50 px-4 pt-4 shadow-lg shadow-slate-500 dark:shadow-slate-800 dark:border-gray-800 dark:bg-black md:rounded-2xl md:px-8 md:pt-8">
+						<h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">Exit Game</h2>
+						<p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+							Are you sure you want to exit the game?  Any progress will be lost.
+						</p>
+						<ExitForm gameId={gameId} firstName={user?.first_name ?? ""}/>
+					</div>
+					:
+						<ToastMessage notifications={[notification]} />
+					}
 			</main>
 			<footer className="w-full h-14 p-4 flex justify-center text-sky-700 dark:text-sky-300">
 				{/* TODO: When the user clicks the Cancel button, go back to the previous page */}
