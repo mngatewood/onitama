28243890d775221b380b834f6b0f1b0dafd11de5
Michael Gatewood <michael@coincloudnv.com>
2025-02-19 21:36:13 -0800

    Update position of background image

--- app/components/Lobby.tsx
+++ app/components/Lobby.tsx
@@ -24,10 +24,10 @@ const Lobby = async () => {
 				<Title />
 			</header>
 			<main className="p-4 overflow-hidden flex flex-col justify-center items-center">
-				<div className="flex items-center justify-center h-full max-w-2xl">
-					<Image src={spirit} width={640} height={640} alt="Spirit" className="absolute top-[calc(50%-300px)] opacity-20 z-[-1]" priority />
+				<div className="flex items-center justify-center absolute w-screen h-screen z-[-1]">
+					<Image src={spirit} width={640} height={640} alt="Spirit" className="absolute opacity-20 z-[-1]" priority />
 				</div>
-					<LobbyForm session={session} initialPendingGames={pendingGames} />
+				<LobbyForm session={session} initialPendingGames={pendingGames} />
 			</main>
 			<footer className="container w-full h-14 p-4 flex justify-center gap-4 text-sky-700 dark:text-sky-300">
 				<button className="w-1/3"><Link href="/logout">Logout</Link></button>
