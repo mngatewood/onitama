669dd606c4b2fd1b8ea1661a764abec7058dc4df
Michael Gatewood <michael@coincloudnv.com>
2025-02-19 21:36:33 -0800

    Style Lobby form

--- app/components/forms/LobbyForm.tsx
+++ app/components/forms/LobbyForm.tsx
@@ -93,42 +93,49 @@ export const LobbyForm = ({session, initialPendingGames}: LobbyFormProps) => {
 				</div>
 				:
 				<div>
-					<h2 className="text-2xl mb-8 font-bold text-neutral-800 dark:text-neutral-200">Game Lobby</h2>
-					<button
-						className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-neutral-500 to-neutral-400 font-medium hover:bg-gradient-to-br hover:from-neutral-600 hover:to-neutral-400 text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:hover:bg-white dark:hover:from-zinc-800 dark:hover:to-zinc-700 dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-800 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] disabled:pointer-events-none disabled:opacity-75"
-						onClick={handleNewGame}
-					>
-						New Game
-						<span
-							className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100"
-						></span>
-						<span
-							className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100"
-						></span>
-					</button>
-					<div className="mt-8">
-						<h2 className="text-2xl mb-4 font-bold text-neutral-800 dark:text-neutral-200">Pending Games</h2>
-						{pendingGames && pendingGames.length > 0 ? (
-							<div className="game-join-list flex flex-col gap-2">
-								{pendingGames.map((game) => (
-									<div key={game.id} className="game-join-entry flex justify-between items-center">
-										<div className="game-host-name">
-											{game.users[0].first_name}
-										</div>
-										<div className="game-created-at">
-											{game.createdAt.toLocaleString()}
-										</div>
-										<button onClick={() => handleJoinGame(game)}>
-											Join
-										</button>
+					<div className="mx-auto overflow-y-scroll scroll-no-bars w-full max-w-md rounded-none border border-gray-300 bg-amber-50 px-4 pt-4 shadow-lg shadow-slate-500 dark:shadow-slate-800 dark:border-gray-800 dark:bg-black md:rounded-2xl md:px-8 md:pt-8">
+						<h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">Game Lobby</h2>
+						<p className="my-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+							Create a new game or join an existing one
+						</p>
+						<button
+							className="group/btn relative block h-10 my-6 w-full rounded-md bg-gradient-to-br from-neutral-500 to-neutral-400 font-medium hover:bg-gradient-to-br hover:from-neutral-600 hover:to-neutral-400 text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:hover:bg-white dark:hover:from-zinc-800 dark:hover:to-zinc-700 dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-800 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] disabled:pointer-events-none disabled:opacity-75"
+							onClick={handleNewGame}
+						>
+							New Game
+							<span
+								className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100"
+							></span>
+							<span
+								className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100"
+							></span>
+						</button>
+						<div>
+							{/* <h2 className="text-lg mb-4 font-bold text-neutral-800 dark:text-neutral-200">Pending Games</h2> */}
+							<div className="flex justify-center items-center min-h-28">
+								{pendingGames && pendingGames.length > 0 ? (
+									<div className="game-join-list flex flex-col gap-2 w-full mb-4 py-2 max-h-48 overflow-y-scroll scroll-no-bars">
+										{pendingGames.map((game) => (
+											<button key={game.id} onClick={() => handleJoinGame(game)} className="game-join-entry group/btn flex justify-between items-center shrink-0 h-10 w-full rounded-md border border-gray-300 bg-amber-50 px-4 shadow-lg shadow-slate-500 dark:shadow-slate-800 dark:border-gray-800 dark:bg-neutral-300 text-gray-800 font-bold dark:hover:bg-neutral-100 hover:text-gray-900 cursor-pointer">
+												<div className="game-host-name w-1/3 text-left group-hover/btn:opacity-50">
+													{game.users[0].first_name}
+												</div>
+												<div className="w-1/3 text-center hidden group-hover/btn:block">
+													Join
+												</div>
+												<div className="game-created-at w-1/3 text-right group-hover/btn:opacity-50">
+													{ new Date(game.createdAt).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true }) }
+												</div>
+											</button>
+										))}
 									</div>
-								))}
+								) : (
+									<div>
+										<p>No pending games to display.</p>
+									</div>
+								)}
 							</div>
-						) : (
-							<div>
-								<p>No pending games to display.</p>
-							</div>
-						)}
+						</div>
 					</div>
 				</div>
 			}
