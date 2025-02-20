85a2618f706f1e440c5a3ab5b30fbf7fcf3f33b8
Michael Gatewood <michael@coincloudnv.com>
2025-02-19 16:27:46 -0800

    Display a loading modal while new game is created

--- app/components/forms/LobbyForm.tsx
+++ app/components/forms/LobbyForm.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { redirect } from "next/navigation";
 import { createGame, joinGame } from "../helpers/lobby";
 import { socket } from "../../lib/socketClient";
-import { Game } from "../Game";
+import { WaitingModal } from "../WaitingModal";
 
 interface LobbyFormProps {
 	session: AppendedSession;
@@ -13,6 +13,7 @@ interface LobbyFormProps {
 export const LobbyForm = ({session, initialPendingGames}: LobbyFormProps) => {
 
 	const [pendingGames, setPendingGames] = React.useState<Game[]>([]);
+	const [loading, setLoading] = React.useState(false);
 
 	useEffect(() => {
 		if (initialPendingGames) {
@@ -24,14 +25,14 @@ export const LobbyForm = ({session, initialPendingGames}: LobbyFormProps) => {
 
 	const handleNewGame = async () => {
 
-		// TODO: Display loading modal
-
 		if(session?.user.id) {
+			setLoading(true);
 			const newGame = await createGame(session?.user.id);
 			if(newGame) {
 				socket.emit("game_created", newGame);
 				redirect(`/play/${newGame.id}`);
 			}
+			setLoading(false);
 		} else {
 			redirect('/login');
 		}
@@ -68,8 +69,9 @@ export const LobbyForm = ({session, initialPendingGames}: LobbyFormProps) => {
 		socket.on("game_full", (filledGameId: string) => {
 			// Remove full game from state
 			setPendingGames((prevGames) => 
+				// Filter out games that are older than 1 hour
 				prevGames.filter(game => 
-					game.createdAt > new Date(Date.now() - 2 * 60 * 1000) &&
+					game.createdAt > new Date(Date.now() - 60 * 60 * 1000) &&
 					game.id !== filledGameId
 				)
 			);
@@ -83,44 +85,53 @@ export const LobbyForm = ({session, initialPendingGames}: LobbyFormProps) => {
 	}, []);
 
 	return (
-		<div>
-			<h2 className="text-2xl mb-8 font-bold text-neutral-800 dark:text-neutral-200">Game Lobby</h2>
-			<button
-				className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-neutral-500 to-neutral-400 font-medium hover:bg-gradient-to-br hover:from-neutral-600 hover:to-neutral-400 text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:hover:bg-white dark:hover:from-zinc-800 dark:hover:to-zinc-700 dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-800 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] disabled:pointer-events-none disabled:opacity-75"
-				onClick={handleNewGame}
-			>
-				New Game
-				<span
-					className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100"
-				></span>
-				<span
-					className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100"
-				></span>
-			</button>
-			<div className="mt-8">
-				<h2 className="text-2xl mb-4 font-bold text-neutral-800 dark:text-neutral-200">Pending Games</h2>
-				{pendingGames && pendingGames.length > 0 ? (
-					<div className="game-join-list flex flex-col gap-2">
-						{pendingGames.map((game) => (
-							<div key={game.id} className="game-join-entry flex justify-between items-center">
-								<div className="game-host-name">
-									{game.users[0].first_name}
-								</div>
-								<div className="game-created-at">
-									{game.createdAt.toLocaleString()}
-								</div>
-								<button onClick={() => handleJoinGame(game)}>
-									Join
-								</button>
+		<>
+			{ loading 
+				?
+				<div className="absolute top-0 left-0 right-0 bottom-0 flex items-center">
+					<WaitingModal text="Loading..." />
+				</div>
+				:
+				<div>
+					<h2 className="text-2xl mb-8 font-bold text-neutral-800 dark:text-neutral-200">Game Lobby</h2>
+					<button
+						className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-neutral-500 to-neutral-400 font-medium hover:bg-gradient-to-br hover:from-neutral-600 hover:to-neutral-400 text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:hover:bg-white dark:hover:from-zinc-800 dark:hover:to-zinc-700 dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-800 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] disabled:pointer-events-none disabled:opacity-75"
+						onClick={handleNewGame}
+					>
+						New Game
+						<span
+							className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100"
+						></span>
+						<span
+							className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100"
+						></span>
+					</button>
+					<div className="mt-8">
+						<h2 className="text-2xl mb-4 font-bold text-neutral-800 dark:text-neutral-200">Pending Games</h2>
+						{pendingGames && pendingGames.length > 0 ? (
+							<div className="game-join-list flex flex-col gap-2">
+								{pendingGames.map((game) => (
+									<div key={game.id} className="game-join-entry flex justify-between items-center">
+										<div className="game-host-name">
+											{game.users[0].first_name}
+										</div>
+										<div className="game-created-at">
+											{game.createdAt.toLocaleString()}
+										</div>
+										<button onClick={() => handleJoinGame(game)}>
+											Join
+										</button>
+									</div>
+								))}
 							</div>
-						))}
+						) : (
+							<div>
+								<p>No pending games to display.</p>
+							</div>
+						)}
 					</div>
-				) : (
-					<div>
-						<p>No pending games to display.</p>
-					</div>
-				)}
-			</div>
-		</div>
+				</div>
+			}
+		</>
 	)
 }
