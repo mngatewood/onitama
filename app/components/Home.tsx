20d0f794fb1e45be2657ecbd9247cba8b6230a6f
Michael Gatewood <michael@coincloudnv.com>
2025-02-27 10:10:14 -0800

    Add hover effects to links and buttons

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -45,9 +45,25 @@ const Home = () => {
 				</div>
 			</main>
 			<footer className="container w-full h-10 portrait:h-14 landscape:short:h-14 p-2 portrait:p-4 landscape:short:p-4 flex justify-center gap-4 text-sky-700 dark:text-sky-300">
-				<button className="w-1/3"><Link href="/login">Login</Link></button>
-				<button className="w-1/3"><Link href="/register">Register</Link></button>
-				<button className="w-1/3"><Link href="/demo">Guide</Link></button>
+				<button className="w-1/3 group hover:font-bold hover:scale-125 transition-all duration-500">
+					<Link href="/login" className="relative">
+						<span>Login</span>
+						<span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-full"></span>
+					</Link>
+				</button>
+				<button className="w-1/3 group hover:font-bold hover:scale-125 transition-all duration-500">
+					<Link href="/register" className="relative">
+						<span>Register</span>
+						<span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-1/2"></span>
+						<span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-1/2"></span>
+					</Link>
+				</button>
+				<button className="w-1/3 group hover:font-bold hover:scale-125 transition-all duration-500">
+					<Link href="/demo" className="relative">
+						<span>Guide</span>
+						<span className="absolute -bottom-1 right-0 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-full"></span>
+					</Link>
+				</button>
 			</footer>
 			<DarkModeToggle />
 			<ToastMessage notifications={notifications}/>
