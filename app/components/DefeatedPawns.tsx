69fefd4abba2743830d1418b1449158a1d5c2b7a
Michael Gatewood <michael@coincloudnv.com>
2025-03-13 12:37:04 -0700

    Fix DefeatedPawns component to only evaluate student pawns when determining defeated pawns to render

--- app/components/DefeatedPawns.tsx
+++ app/components/DefeatedPawns.tsx
@@ -10,21 +10,23 @@ export const DefeatedPawns = ( {board}: { board: string[][] | null } ) => {
 	useEffect(() => {
 		if (!board) return
 		const flatBoard = board.flat();
-		const redPawns = flatBoard.filter((row) => ["rs", "rm"].includes(row.slice(0, 2))).length;
-		const bluePawns = flatBoard.filter((row) => ["bs", "bm"].includes(row.slice(0, 2))).length;
-		setDefeatedRedPawns(5 - redPawns);
-		setDefeatedBluePawns(5 - bluePawns);
+		const redPawns = flatBoard.filter((row) => row.slice(0, 2) === "rs").length;
+		const bluePawns = flatBoard.filter((row) => row.slice(0, 2) === "bs").length;
+		setDefeatedRedPawns(4 - redPawns);
+		setDefeatedBluePawns(4 - bluePawns);
 	}, [board])
 	
 
 	return (
 		<div id="defeated-pawns" className="flex flex-col space-between items-end w-full h-full">
 			<div className="flex flex-col justify-evenly w-1/5 min-w-6 short:sm:min-w-8 h-full">
+				{/* defeated red pawns */}
 				{[...Array(defeatedRedPawns).keys()].map((index) => {
 					return (
 						<div key={index} className="defeated red defeated-pawn aspect-square"></div>
 					)
 				})}
+				{/* red placeholders */}
 				{[...Array(4 - defeatedRedPawns).keys()].map((index) => {
 					return (
 						<div key={index} className="defeated sil defeated-pawn aspect-square"></div>
@@ -32,11 +34,13 @@ export const DefeatedPawns = ( {board}: { board: string[][] | null } ) => {
 				})}
 			</div>
 			<div className="flex flex-col justify-evenly w-1/5 min-w-6 short:sm:min-w-8 h-full">
+				{/* defeated blue pawns */}
 				{[...Array(defeatedBluePawns).keys()].map((index) => {
 					return (
 						<div key={index} className="defeated blue defeated-pawn aspect-square"></div>
 					)
 				})}
+				{/* blue placeholders */}
 				{[...Array(4 - defeatedBluePawns).keys()].map((index) => {
 					return (
 						<div key={index} className="defeated sil defeated-pawn aspect-square"></div>
