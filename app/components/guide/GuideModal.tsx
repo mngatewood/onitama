dea17a6bb57e96228408b0509dea45d8e8361272
Michael Gatewood <michael@coincloudnv.com>
2025-03-26 14:41:19 -0700

    Wait for position before rendering modal

--- app/components/guide/GuideModal.tsx
+++ app/components/guide/GuideModal.tsx
@@ -18,6 +18,7 @@ export const GuideModal = ({ modal, stage, updateStage }: GuideModalProps) => {
 	const heading = modal.headline;
 	const [position, setPosition] = React.useState<string>("upOrLeft");
 	const [positionStyle, setPositionStyle] = React.useState<object>({});
+	const [modalReady, setModalReady] = React.useState<boolean>(false);
 
 	let positionClass;
 
@@ -29,6 +30,7 @@ export const GuideModal = ({ modal, stage, updateStage }: GuideModalProps) => {
 		} else if (modal.position.y > 0) {
 			setPositionStyle({ top: `${modal.position.y}%` });
 		}
+		setModalReady(true);
 	}, [modal.position])
 	
 
@@ -57,7 +59,7 @@ export const GuideModal = ({ modal, stage, updateStage }: GuideModalProps) => {
 
 	return (
 		<div className={`${positionClass} h-screen w-screen mx-auto transition-all duration-500 ease-in-out`}>
-			<div style={positionStyle} className="relative top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center landscape:max-w-[30%] w-3/4 mt-6 p-4 border border-neutral-800 text-neutral-800 bg-gray-200 shadow-[2px_2px_5px_0px_rgba(0,0,0,0.7)] dark:shadow-[2px_2px_5px_0px_rgba(255,255,255,0.7)] rounded-2xl">
+			<div style={positionStyle} className={`${modalReady ? "opacity-1" : "opacity-0"} relative top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center landscape:max-w-[30%] w-3/4 mt-6 p-4 border border-neutral-800 text-neutral-800 bg-gray-200 shadow-[2px_2px_5px_0px_rgba(0,0,0,0.7)] dark:shadow-[2px_2px_5px_0px_rgba(255,255,255,0.7)] rounded-2xl transition-opacity duration-500 ease-in-out`}>
 				<div className="relative top-0 left-1/2 w-0 h-0 flex justify-center items-center">
 
 					{modal.moveable &&
