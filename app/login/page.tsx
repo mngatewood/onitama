ae008866fa35266a96b41012263b865cfb162005
Michael Gatewood <michael@coincloudnv.com>
2025-03-29 12:49:08 -0700

    Update login and register footer nav link text

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -33,7 +33,7 @@ const Login = async () => {
 			<footer className="w-full h-10 portrait:h-14 landscape:short:h-14 p-2 portrait:p-4 landscape:short:p-4 flex justify-center gap-4 text-sky-700 dark:text-sky-300 bg-neutral-200 dark:bg-blue-1 z-50">
 				<button className="w-1/3 group hover:font-bold hover:scale-125 transition-all duration-500">
 					<TransitionLink href="/" className="relative">
-						<span>Cancel</span>
+						<span>Home</span>
 						<span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-1/2"></span>
 						<span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-sky-700 dark:bg-sky-300 group-hover:w-1/2"></span>
 					</TransitionLink>
